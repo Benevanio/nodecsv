68597b060d33e83a6f19b6b0f848468bad6d7ca3
Bene-Tesla <benevaniosantos930@gmail.com>
2024-04-28 16:12:46 -0300

    :tada: Adicionar classe Processor para processamento de dados

---
+++ Processor.js
@@ -0,0 +1,13 @@
+class Processor {
+    static Process(data) {
+        var a = data.split("\r\n");
+        var rows = [];
+        a.forEach(element => {
+            var arr = element.split(",");
+            rows.push(arr);
+        });
+        return rows;
+    }
+}
+
+module.exports = Processor;
