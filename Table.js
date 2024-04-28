10f919e47e63dccec1d75af9a210844338d915ba
Bene-Tesla <benevaniosantos930@gmail.com>
2024-04-28 16:13:35 -0300

    :tada: Adicionar classe Table para representação de tabelas

---
+++ Table.js
@@ -0,0 +1,14 @@
+// Table.js
+class Table {
+    constructor(arr) {
+        this.header = arr[0];
+        arr.shift();
+        this.rows = arr;
+    }
+    get RowCount() {
+        return this.rows.length;
+    }
+    
+}
+
+module.exports = Table;
